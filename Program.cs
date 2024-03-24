1615cf8b2f5e146ea29b5a5916a59f23259369ba
Carlos-Cristiano917 <87499949+Carlos-Cristiano917@users.noreply.github.com>
2024-03-24 14:09:08 -0300

    Add files via upload
    Algoritmo que exibe horas / minutos / segundos

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace convertendo_segundos_horas_minutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundos, resto, h, m, s;
+            Console.WriteLine("Informe a quantidade de segundos:");
+            segundos = Convert.ToInt32(Console.ReadLine());
+            h = segundos / 3600;
+            resto = segundos % 3600;
+            m = resto / 60;
+            s = resto % 60;
+            Console.WriteLine("São {0} Horas, {1} minutos e {2} segundos.", h, m, s);
+    }
+}
+}
